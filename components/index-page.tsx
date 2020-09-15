09bddb1021a77783c0309f17d45b3283fccc7b80
heegupark <slhead@gmail.com>
2020-09-14 23:06:13 -0700

    initial setup

--- components/index-page.tsx
+++ components/index-page.tsx
@@ -24,12 +24,6 @@ export default function IndexPage() {
           type="image/png"
           href="../static/images/o-logo.png"
         />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"
-          integrity="sha256-+N4/V/SbAFiW1MPBCXnfnP9QSN3+Keu+NlB+0ev/YKQ="
-          crossOrigin="anonymous"
-        />
       </Head>
     </>
   );
